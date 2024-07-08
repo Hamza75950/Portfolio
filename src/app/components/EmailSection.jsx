664cbdc08b3b32ec390587761838a80570f5952b
Hamza75950 <hamza.niazi004@gmail.com>
2024-07-08 14:04:35 +0500

    Portfolio website

--- src/app/components/EmailSection.jsx
+++ src/app/components/EmailSection.jsx
@@ -56,10 +56,10 @@ const EmailSection = () => {
           try my best to get back to you!
         </p>
         <div className="socials flex flex-row gap-2">
-          <Link href="github.com">
+          <Link href="https://github.com/Hamza75950">
             <Image src={GithubIcon} alt="Github Icon" />
           </Link>
-          <Link href="linkedin.com">
+          <Link href="https://www.linkedin.com/in/hamza-khan-90aa27257">
             <Image src={LinkedinIcon} alt="Linkedin Icon" />
           </Link>
         </div>
--- src/app/components/HeroSection.jsx
+++ src/app/components/HeroSection.jsx
@@ -21,26 +21,16 @@ const HeroSection = () => {
             </span>
             <br></br>
             <TypeAnimation
-              sequence={[
-                "Judy",
-                1000,
-                "Web Developer",
-                1000,
-                "Mobile Developer",
-                1000,
-                "UI/UX Designer",
-                1000,
-              ]}
+              sequence={["Hamza Khan", 1000, "Web Developer", 1000]}
               wrapper="span"
               speed={50}
               repeat={Infinity}
             />
           </h1>
           <p className="text-[#ADB7BE] text-base sm:text-lg mb-6 lg:text-xl">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam,
-            voluptuous.
+            I am passionate about coding MERN Stack and full stack development.
           </p>
-          <div>
+          {/* <div>
             <Link
               href="/#contact"
               className="px-6 inline-block py-3 w-full sm:w-fit rounded-full mr-4 bg-gradient-to-br from-primary-500 to-secondary-500 hover:bg-slate-200 text-white"
@@ -55,7 +45,7 @@ const HeroSection = () => {
                 Download CV
               </span>
             </Link>
-          </div>
+          </div> */}
         </motion.div>
         <motion.div
           initial={{ opacity: 0, scale: 0.5 }}
@@ -63,13 +53,13 @@ const HeroSection = () => {
           transition={{ duration: 0.5 }}
           className="col-span-4 place-self-center mt-4 lg:mt-0"
         >
-          <div className="rounded-full bg-[#181818] w-[250px] h-[250px] lg:w-[400px] lg:h-[400px] relative">
+          <div className="w-[250px] h-[250px] md:ml-56 lg:w-[700px] lg:h-[700px] relative">
             <Image
-              src="/images/hero-image.png"
+              src="/images/pose_52.png"
               alt="hero image"
               className="absolute transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
-              width={300}
-              height={300}
+              width={700}
+              height={700}
             />
           </div>
         </motion.div>
