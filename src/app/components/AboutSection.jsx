998106bef2126cd3f5fbf8b9aa30b4e0e1110876
Hamza75950 <hamza.niazi004@gmail.com>
2024-07-11 19:29:26 +0500

    4th commit

--- src/app/components/AboutSection.jsx
+++ src/app/components/AboutSection.jsx
@@ -58,6 +58,7 @@ const AboutSection = () => {
           src="/images/pose_52.png"
           width={500}
           height={500}
+          alt=""
         />
         <div className="mt-4 md:mt-0 text-left flex flex-col h-full">
           <h2 className="text-4xl font-bold text-white mb-4">About Me</h2>
